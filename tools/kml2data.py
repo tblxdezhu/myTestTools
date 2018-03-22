c5c2e3fb1af9667cb8a88e81d6741343e0cd37d6
Zhenxuan Xu <Zhenxuan.Xu@ygomi.com>
2018-03-22 14:38:56 +0800

    test kml2data

--- tools/kml2data.py
+++ tools/kml2data.py
@@ -100,12 +100,11 @@ def data_process():
 
 def draw(mode):
     data, center_data = data_process()
-    backup_path = os.path.join(sys.path[0], "webkmls")
+    backup_path = os.path.join(sys.path[0], "webkmls", mode)
     if os.path.exists(backup_path):
         os.system("rm -rf " + backup_path + "/*")
-        os.system("mkdir " + backup_path + "/" + mode)
     else:
-        os.system("mkdir -p" + backup_path + "/" + mode)
+        os.system("mkdir -p" + backup_path)
     allinone_path = os.path.join(sys.path[0], "webkmls", mode, mode + "_all_in.html")
     with open(allinone_path, 'w') as f:
         f.write(
