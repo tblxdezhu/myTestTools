f316c1f0cbf3c3fb49f313ffbe550939d985ecd6
Zhenxuan Xu <Zhenxuan.Xu@ygomi.com>
2018-04-03 13:15:51 +0800

    add result_analyse

--- Analyse/result_analyse.py
+++ Analyse/result_analyse.py
@@ -241,7 +241,8 @@ def backup_diff(cases_path, diff_dic):
                     if case in diff_dic[mode]:
                         pass
                     else:
-                        print os.path.dirname(cases_path)
+                        if cases_path.endswith("/"):
+                            cases_path = os.path.dirname(cases_path)
                         print "mkdir ", os.path.join(os.path.dirname(cases_path), "backup_diff_cases")
                         # os.system("mkdir " + os.path.join(os.path.dirname(cases_path), "backup_diff_cases")
 
