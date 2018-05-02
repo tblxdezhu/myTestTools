7cb2aeb73239a93af42f1aa849397eae34df10f5
Zhenxuan Xu <Zhenxuan.Xu@ygomi.com>
2018-05-02 13:47:22 +0800

    add Xran

--- Run/Xrun_ZSLAM_in_debug.py
+++ Run/Xrun_ZSLAM_in_debug.py
@@ -7,6 +7,8 @@
 # @Software: PyCharm
 
 
+#To run Zslam in debug
+
 import os
 import sys
 from optparse import OptionParser
@@ -21,61 +23,71 @@ import logging
 import datetime
 import coloredlogs
 
-
-def vehcile_slam(path):
+def vehcile_slam(o_path,i_config):
     pool = Pool(3)
     for rtv in rtvs:
-        for imu in imus:
-            if os.path.basename(rtv).strip('.rtv') == os.path.basename(imu).strip(".imu"):
-                # print  os.path.basename(rtv)
-                # print os.path.join(path,os.path.basename(rtv).strip(".rtv"))
-                if os.path.exists(os.path.join(path, os.path.basename(rtv).strip(".rtv"))):
-                    continue
-                else:
-                    os.mkdir(os.path.join(path, os.path.basename(rtv).strip(".rtv")))
-                    output_dir = os.path.join(path, os.path.basename(rtv).strip(".rtv"))
-                    # os.chdir(output_dir)
-                    # print output_dir
-                    pool.apply_async(run_cmd, (rtv, imu, gps, output_dir))
-                # print rtv
-                # print imu
+        imu=rtv[:-3]+'imu'
+        if imu in imus:
+            date_time=str(datetime.datetime.now().month)+'_'+str(datetime.datetime.now().day)
+            output_dir = os.path.join(o_path, date_time + '_' + os.path.basename(rtv).strip(".rtv"))
+            #print  os.path.basename(rtv)
+            #print os.path.join(path,os.path.basename(rtv).strip(".rtv"))
+            if os.path.exists(output_dir):
+                continue
+            else:
+                os.mkdir(output_dir)
+                gps= os.path.basename(rtv).strip(".rtv")+'.txt'
+                # os.system('touch '+output_dir+gps)
+                # os.chdir(output_dir)
+                # print output_dir
+                pool.apply_async(run_cmd,(rtv,imu,os.path.join(output_dir,gps),output_dir,i_config))
+            # print rtv
+            # print imu
     pool.close()
     pool.join()
 
-
-def get_files(type, path_rtv):
-    find_cmd = ' find ' + path_rtv + ' -name ' + type
+def get_files(type,path_rtv):
+    find_cmd=' find '+path_rtv +' -name '+type
     status, files = commands.getstatusoutput(find_cmd)
     file = files.split('\n')
-    # print file
+    #print file
     return file
 
-
-def run_cmd(rtv, imu, gps, path):
+def run_cmd(rtv,imu,gps,path,in_config):
     os.chdir(path)
-    exe = os.path.join('/home/', user_name, '/source/core/algorithm_vehicle/vehicle/offlineSLAM/bin/ZSLAMExe')
-    config = os.path.join('/home/', user_name,
-                          '/source/core/algorithm_vehicle/vehicle/offlineSLAM/config/config65.yaml')
-    run_cmd_list = [exe, '--rtv', rtv, '--iimu', imu, '--igps', gps, '--ip', config, '--ic', path, '--d', path]
-    run_cmds = ' '.join(run_cmd_list)
+    exe=os.path.join('/home',name,'source/core/algorithm_vehicle/vehicle/offlineSLAM/bin/ZSLAMExe')
+    config=os.path.join('/home',name,'source/core/algorithm_vehicle/vehicle/offlineSLAM/config',in_config)
+    run_cmd_list = [exe,'--rtv',rtv,'--iimu',imu,'--igps',gps,'--ip',config,'--ic',path,'--d',path]
+    run_cmds=' '.join(run_cmd_list)
     print run_cmds
-    # b = os.system(run_cmds)
+    b=os.system(run_cmds)
+
+
+def get_parser():
+
+    parser = OptionParser(usage="%prog [-n] [--ip] [--op] [-c]", version="%prog 1.0")
+    parser.add_option("-n", dest="name",
+                      help="Select the username:'user' or 'roaddb'",
+                      default="roaddb")
+    parser.add_option("--ip", dest="in_path",
+                      help="Select your in_path")
+    parser.add_option("--op", dest="out_path",
+                      help="Select the out_path",)
+    parser.add_option("-c", dest="config",
+                      help="Select config file",)
+    (options, args) = parser.parse_args()
+    if options.config == None:
+        print parser.error("You must specify a configuration file")
+    return options.name, options.in_path, options.out_path, options.config
 
 
 if __name__ == '__main__':
-    global rtvs
-    global imus
-    global path
-    global gps
-    global user_name
-    gps = ''
-    rtvs = get_files('*.rtv', sys.argv[2])
-    imus = get_files('*.imu', sys.argv[2])
-    path = sys.argv[3]
-    user_name = sys.argv[1]
-    print user_name
-    gps = os.path.join('/home', user_name, '/myTestTools/Run/a.txt')
-    print gps
-    # vehcile_slam(path)
-    # print rtvs
-    # print imus
+    global name
+    # global gps
+    name,in_path,out_path,config=get_parser()
+    rtvs=get_files('*.rtv',in_path)
+    imus=get_files('*.imu',in_path)
+    # gps = os.path.join('/home',name,'myTestTools/Run/a.txt')
+    vehcile_slam(out_path,config)
+    #print rtvs
+    #print imus
